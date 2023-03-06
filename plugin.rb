18e83a1ba38b8ef3f0a30f4c6d5459cf34fa2b13
Dmitrii Fediuk <dfediuk@gmail.com>
2023-03-06 09:19:00 +0300

    https://github.com/tr-guide/site/issues/1

--- plugin.rb
+++ plugin.rb
@@ -3,3 +3,11 @@
 # version: 0.0.1
 # authors: Dmitrii Fediuk
 # url: https://www.upwork.com/fl/mage2pro
+after_initialize do
+  # 2023-03-06 "Allow posts without body": https://github.com/tr-guide/site/issues/1
+	require 'post_validator'
+	PostValidator.class_eval do
+    def raw_quality(post)
+    end
+	end
+end
