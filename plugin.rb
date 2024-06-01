8632b7ee9dfa928ccae2416d2deb0da19fde3c82
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-01 10:35:08 +0100

    https://github.com/discourse-pro/df-qa/issues/3

--- plugin.rb
+++ plugin.rb
@@ -1,13 +1,18 @@
 # name: df-qa
 # about: A plugin for question / answer websites.
-# version: 0.0.1
+# version: 0.0.2
 # authors: Dmitrii Fediuk
-# url: https://www.upwork.com/fl/mage2pro
+# url: https://upwork.com/fl/mage2pro
 after_initialize do
 	# 2023-03-06 "Allow posts without body": https://github.com/tr-guide/site/issues/1
 	require 'post_validator'
 	PostValidator.class_eval do
 		def raw_quality(post)
+		end
+		# 2024-06-01
+		# 1) "Allow posts without body": https://github.com/tr-guide/site/issues/1
+		# 2) «Body can't be blank»: https://github.com/discourse-pro/df-qa/issues/3
+		def stripped_length(post)
 		end
 	end
 end
